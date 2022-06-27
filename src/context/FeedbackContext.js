41593284e433ef92f44900323a35caf72e6f1a7c
Joseph Valle <jvcarrolljr@gmail.com>
2022-06-27 01:27:37 -0400

    Completed project

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import {createContext, useState, useEffect} from 'react'
-import {v4 as uuidv4} from 'uuid'
 
 const FeedbackContext = createContext()
 
@@ -17,21 +16,33 @@ export const FeedbackProvider = ({children}) => {
   },[])
 
   const fetchFeedback = async () => {
-    const response = await fetch("http://localhost:5000/feedback?_sort=id&order_desc")
+    const response = await fetch("/feedback?_sort=id&order_desc")
     const data = await response.json()
     setFeedback(data)
     setIsLoading(false)
   }
 
   // Add feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback]) 
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    })
+    
+    const data = await response.json()
+
+    setFeedback([data, ...feedback]) 
   }
 
   // Delete feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if(window.confirm('Are you sure you want to delete?')) {
+
+      await fetch(`/feedback/${id}`, {method:'DELETE'})
+
       setFeedback(feedback.filter((item) => item.id !== id))
     } 
   }
@@ -44,8 +55,19 @@ export const FeedbackProvider = ({children}) => {
     })
   }
 
-  const updateFeedback = (id, updItem) => {
-    setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem } : item))
+  const updateFeedback = async (id, updItem) => {
+
+    const response = await fetch(`/feedback/${id}`, {
+      method:'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updItem)
+    })
+
+    const data = await response.json()
+
+    setFeedback(feedback.map((item) => item.id === id ? { ...item, ...data } : item))
   }
 
   return <FeedbackContext.Provider value={{
